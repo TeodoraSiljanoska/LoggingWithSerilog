8e02a2b4e115ca3e51d1a5c88ff054697c42c25e
Teodora <siljanoska.teodora@uklo.edu.mk>
2023-07-19 16:47:39 +0200

    Add project files.

---
+++ LoggingWithSerilog/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using LoggingWithSerilog.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Serilog.AspNetCore;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace LoggingWithSerilog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ILogger<StudentController> logger;
+
+        public StudentController(ILogger<StudentController> logger) 
+        { 
+            this.logger = logger;
+        }
+
+        [HttpGet(Name = "GetStudent")]
+        public IEnumerable<Student> Get()
+        {
+            //Testing Exception here
+            if (true)
+            {
+              throw new Exception("Failed to retrieve data");
+            }
+
+            //Testing Logs Here
+            var response = Enumerable.Range(1, 1).Select(index => new Student
+            {
+                Address = "Taxus USA",
+                GraduationYear = 2023,
+                Name = "Teodora"
+            }).ToArray();
+            logger.LogDebug("Inside GetStudent endpoint");
+            logger.LogDebug($"The response for the GetStudent is {JsonConvert.SerializeObject(response)}");
+            
+            return response;
+        }
+    }
+}
---
+++ LoggingWithSerilog/Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace LoggingWithSerilog.Model
+{
+    public class Student
+    {
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public int GraduationYear { get; set; }
+        public string? Address { get; set; }
+    }
+}
